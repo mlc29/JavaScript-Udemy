4637ef3175311916186277024dc9171b904e5179
mlc29 <marcuscisco2023@u.northwestern.edu>
2023-07-04 12:33:25 -0500

    Fundamentals Part 1 Complete

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,35 @@
+/* // Activating Strict Mode Lecture
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 534;
+*/
+
+// Functions Lecture
+function logger() {
+    console.log('My name is Jonas');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
